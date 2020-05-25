e33315b1b334daf4ed19d748884cb79838252239
Madhav Nagpal <mr.madhavnagpal@gmail.com>
2020-05-25 18:38:57 +0530

    test added for comment controllers
    Signed-off-by: Madhav Nagpal <mr.madhavnagpal@gmail.com>

--- src/controllers/comments.js
+++ src/controllers/comments.js
@@ -1,26 +1,18 @@
 const { Posts, Users, Comments } = require("../db/models");
 
 async function createComment(user_id, post_id, comment_body) {
-  try {
-    let user = await Users.findOne({
-      where: {
-        id: user_id,
-      },
-    });
-    console.log("=============================================");
-    console.log("going to create comment");
-    console.log("=============================================");
+  let user = await Users.findOne({
+    where: {
+      id: user_id,
+    },
+  });
 
-    return await Comments.create({
-      body: comment_body,
-      title: user.username,
-      userId: user_id,
-      postId: post_id,
-    });
-  } catch (e) {
-    console.log(e);
-    return null;
-  }
+  return await Comments.create({
+    body: comment_body,
+    title: user.username,
+    userId: user_id,
+    postId: post_id,
+  });
 }
 module.exports = {
   createComment,
