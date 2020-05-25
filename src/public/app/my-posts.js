fc32ef10418e0588286bb1cc28d132eb29570713
Madhav Nagpal <mr.madhavnagpal@gmail.com>
2020-05-25 17:21:30 +0530

    repaired my-post.html:
    Signed-off-by: Madhav Nagpal <mr.madhavnagpal@gmail.com>

--- src/public/app/my-posts.js
+++ src/public/app/my-posts.js
@@ -1,27 +1,46 @@
 function loadMyPosts() {
-  const userId = JSON.parse(window.localStorage.user).id
+  const userId = JSON.parse(window.localStorage.user).id;
 
   $.get(`/api/posts?userId=${userId}`, (posts) => {
     for (let p of posts) {
-      $('#posts-container').append(
-        $(`
-        <div class="col-4">
-          <div class="card m-2">
-            <div class="card-body">
-              <h5 class="card-title">${p.title}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
-              <p class="card-text">
-                ${p.body.substr(0, 200)}
-                <a href="#">...read more</a>
-              </p>
-              <a href="#" class="card-link">Comment</a>
-              <a href="#" class="card-link">Like</a>
-            </div>
+      let item = $(`
+      <div class="col-4">
+        <div class="card m-2">
+          <div class="card-body">
+            <h5 class="card-title">${p.title}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
+            <p class="card-text">
+              ${p.body.substr(0, 200)}
+              <a href="#">...read more</a>
+            </p>
+            <input type="text" placeholder="add suggestions" class="newComment">
+            <button class="card-link btnComment">Comment</button>  
+            <ul class="comment"></ul> 
           </div>
         </div>
-        
-        `)
-      )
+      </div>   
+  `);
+      let commentBox = item.find(".comment");
+      for (let comment of p.comments) {
+        commentBox.append(
+          $("<li></li>").text(`[${comment.title}] : ${comment.body}`)
+        );
+      }
+
+      item.find(".btnComment").on("click", () => {
+        $.post(
+          "/api/comments",
+          {
+            post_id: p.id,
+            comment_body: item.find(".newComment").val(),
+            user_id: JSON.parse(window.localStorage.user).id,
+          },
+          (comment) => {
+            $("#content").load(`/components/my-posts.html`);
+          }
+        );
+      });
+      $("#posts-container").append(item);
     }
-  })
+  });
 }
