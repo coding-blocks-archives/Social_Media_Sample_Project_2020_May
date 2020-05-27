d63c5ecbab3dc19674f843cbc7923efc1b62028b
blackhatabhi <abhinavpnp.kumar@gmail.com>
2020-05-27 20:00:28 +0530

    making routes for my comments
    Signed-off-by: blackhatabhi <abhinavpnp.kumar@gmail.com>

--- src/routes/posts/comments.js
+++ src/routes/posts/comments.js
@@ -1,24 +1,21 @@
 const route = require("express").Router();
 const { createComment } = require("../../controllers/comments");
 
-//create comments
 route.post("/", async (req, res) => {
   try {
     let { user_id, post_id, comment_body } = req.body;
     if (!user_id || !post_id || !comment_body) {
       res.status(403).send("Bad Request");
     } else {
-      console.log("=============================================");
       console.log("going to create comment");
       console.log(user_id, post_id, comment_body);
-      console.log("=============================================");
 
       let comment = await createComment(user_id, post_id, comment_body);
 
       if (comment) {
         res.status(201).send(comment);
       } else {
-        res.status(501).send("not created Please try again");
+        res.status(501).send("please try again...not created yet");
       }
     }
   } catch (e) {
@@ -26,7 +23,6 @@ route.post("/", async (req, res) => {
   }
 });
 
-// get all comments of a post id
 route.get("/:post_id", async (req, res) => {
   try {
     let allComments = await Comments.findAll({
